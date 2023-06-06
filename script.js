1dc9f17a687684e53aa230f52a8260607e85641d
fdl <mfm474@uowmail.edu.au>
2023-06-06 12:49:40 +0400

    prepaid post paid switch akan pattum

--- script.js
+++ script.js
@@ -18,6 +18,9 @@ getdata();
 function postPaid(){
     var arr = ["Data:","Voice:","Price:","Subscrition Period:"]
     var select = document.querySelectorAll('.mobile__card')
+    for(var i=0;i<select.length;i++){
+        select[i].innerHTML =""
+    }
     for(var i=0;i<dataObj.Services.Mobile_Services.PostPaid.length;i++){
         var j=0
         select[i].innerHTML+= "<h3>"+dataObj.Services.Mobile_Services.PostPaid[i].Name+"</h3>\n"
@@ -38,25 +41,67 @@ function prePaid(){
     var arr = ["Data:","Voice:","Price:","Subscrition Period:"]
     var select = document.querySelectorAll('.mobile__card');
     for(var i=0;i<select.length;i++){
-        select[i].innerHTML +=""
+        select[i].innerHTML =""
     }
-    // for(var i=0;i<dataObj.Services.Mobile_Services.PrePaid.length;i++){
-    //     var j=0
-    //     select[i].innerHTML+="<h3>"+arr[j++]+"</h3>\n"
-    //     select[i].innerHTML+="<p>"+dataObj.Services.Mobile_Services.PrePaid[i].Data+"</p\n>"
-    //     select[i].innerHTML+="<h3>"+arr[j++]+"</h3>\n"
-    //     select[i].innerHTML+="<p>"+dataObj.Services.Mobile_Services.PrePaid[i].Voice+"</p>\n"
-    //     select[i].innerHTML+="<h3>"+arr[j++]+"</h3>"
-    //     select[i].innerHTML+="<p>"+dataObj.Services.Mobile_Services.PrePaid[i].Price+"</p>\n"
-    //     select[i].innerHTML+="<h3>"+arr[j++]+"</h3>"
-    //     select[i].innerHTML+="<p>"+dataObj.Services.Mobile_Services.PrePaid[i].Time+"</p>\n"
-    //     select[i].innerHTML+="<div class=\"sub__btn--right\"><button class=\"subscription__btn\">Subscribe</button></div>"      
-    // }
+    for(var i=0;i<dataObj.Services.Mobile_Services.PrePaid.length;i++){
+        var j=0
+        select[i].innerHTML+="<h3>"+arr[j++]+"</h3>\n"
+        select[i].innerHTML+="<p>"+dataObj.Services.Mobile_Services.PrePaid[i].Data+"</p\n>"
+        select[i].innerHTML+="<h3>"+arr[j++]+"</h3>\n"
+        select[i].innerHTML+="<p>"+dataObj.Services.Mobile_Services.PrePaid[i].Voice+"</p>\n"
+        select[i].innerHTML+="<h3>"+arr[j++]+"</h3>"
+        select[i].innerHTML+="<p>"+dataObj.Services.Mobile_Services.PrePaid[i].Price+"</p>\n"
+        select[i].innerHTML+="<h3>"+arr[j++]+"</h3>"
+        select[i].innerHTML+="<p>"+dataObj.Services.Mobile_Services.PrePaid[i].Time+"</p>\n"
+        select[i].innerHTML+="<div class=\"sub__btn--right\"><button class=\"subscription__btn\">Subscribe</button></div>"      
+    }
     
 
 }
-
-
+function tv(){
+    var arr = ["Data:","Voice:","Price:","Subscrition Period:"]
+    var select = document.querySelectorAll('.mobile__card')
+    for(var i=0;i<select.length;i++){
+        select[i].innerHTML =""
+    }
+    for(var i=0;i<dataObj.Services.Mobile_Services.PostPaid.length;i++){
+        var j=0
+        select[i].innerHTML+= "<h3>"+dataObj.Services.Mobile_Services.PostPaid[i].Name+"</h3>\n"
+        select[i].innerHTML+="<h3>"+arr[j++]+"</h3>\n"
+        select[i].innerHTML+="<p>"+dataObj.Services.Mobile_Services.PostPaid[i].Data+"</p\n>"
+        select[i].innerHTML+="<h3>"+arr[j++]+"</h3>\n"
+        select[i].innerHTML+="<p>"+dataObj.Services.Mobile_Services.PostPaid[i].Voice+"</p>\n"
+        select[i].innerHTML+="<h3>"+arr[j++]+"</h3>"
+        select[i].innerHTML+="<p>"+dataObj.Services.Mobile_Services.PostPaid[i].Price+"</p>\n"
+        select[i].innerHTML+="<h3>"+arr[j++]+"</h3>"
+        select[i].innerHTML+="<p>"+dataObj.Services.Mobile_Services.PostPaid[i].Time+"</p>\n"
+        select[i].innerHTML+="<div class=\"sub__btn--right\"><button class=\"subscription__btn\">Subscribe</button></div>"      
+    }
+    
+    
+}
+function wifi(){
+    var arr = ["Data:","Voice:","Price:","Subscrition Period:"]
+    var select = document.querySelectorAll('.mobile__card')
+    for(var i=0;i<select.length;i++){
+        select[i].innerHTML =""
+    }
+    for(var i=0;i<dataObj.Services.Mobile_Services.PostPaid.length;i++){
+        var j=0
+        select[i].innerHTML+= "<h3>"+dataObj.Services.Mobile_Services.PostPaid[i].Name+"</h3>\n"
+        select[i].innerHTML+="<h3>"+arr[j++]+"</h3>\n"
+        select[i].innerHTML+="<p>"+dataObj.Services.Mobile_Services.PostPaid[i].Data+"</p\n>"
+        select[i].innerHTML+="<h3>"+arr[j++]+"</h3>\n"
+        select[i].innerHTML+="<p>"+dataObj.Services.Mobile_Services.PostPaid[i].Voice+"</p>\n"
+        select[i].innerHTML+="<h3>"+arr[j++]+"</h3>"
+        select[i].innerHTML+="<p>"+dataObj.Services.Mobile_Services.PostPaid[i].Price+"</p>\n"
+        select[i].innerHTML+="<h3>"+arr[j++]+"</h3>"
+        select[i].innerHTML+="<p>"+dataObj.Services.Mobile_Services.PostPaid[i].Time+"</p>\n"
+        select[i].innerHTML+="<div class=\"sub__btn--right\"><button class=\"subscription__btn\">Subscribe</button></div>"      
+    }
+    
+    
+}
 
 
 
